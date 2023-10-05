513ec56a95626975d676a8158fda5e2b62a3a441
DinisSkizee <dinissilvauk@gmail.com>
2023-10-05 14:12:41 +0100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import { useState } from "react";
 import { Footer } from "./components/Global/Footer";
 import { Header } from "./components/Global/Header";
 import { LeftSider } from "./components/Global/LeftSider";
-import { RightSider } from "./components/RightSider";
+import { RightSider } from "./components/Global/RightSider";
 import { AboutMe } from "./pages/AboutMe";
 import { MainPage } from "./pages/MainPage";
 import Resume from "./pages/Resume";
-import PreSite from "./components/PreSite";
+import PreSite from "./components/Global/PreSite";
 
 const App = () => {
   const [valid, setValid] = useState(false);
